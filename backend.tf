80a3edd982f84166d50b6725cc79fa8bc1d2f7a2
Sanjay Pramod <86740453+Sanjaypramod@users.noreply.github.com>
2024-03-12 13:12:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-        bucket = "sanjaypocbucket"
-        key     = "sanjaypocbucket/terraform.tfstate"
-        region = "us-west-2"
+        bucket = "sanjaypocbucket1"
+        key     = "sanjaypocbucket1terraform.tfstate"
+        region = "ap-south-1"
   }
 }
